2e8bbe45862837254df137be37913192cdbe12b0
Casey K. Davis <casey@caseykdavis.com>
2018-10-09 16:14:34 -0400

    Initial Push

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 //=require ../../node_modules/jquery/dist/jquery.min.js
 //=require ../../node_modules/bootstrap/dist/js/bootstrap.min.js
 
-$(function(){
+$(document).ready(function() {
+
   console.log('jquery & bootstrap are ready');
-})
+  console.log('working');
+
+  $('img').addClass('img-responsive');
+
+});
